8c2e729e8765708ecc230c0ee3a983c310ec61f9
Teko <tekofxx@gmail.com>
2026-03-02 12:46:34 +0100

    Created errors custom type

---
+++ internal/errors/errors.go
@@ -0,0 +1,96 @@
+package merrors
+
+type MError struct {
+	Code    MErrorCode `json:"code,omitempty"`
+	Message string     `json:"message,omitempty"`
+}
+type MErrorCode int
+
+func New(code MErrorCode, message string) *MError {
+	return &MError{
+		Code:    code,
+		Message: message,
+	}
+}
+
+const (
+	// 0 --> 999 | SYSTEM UNEXPECTED ERRORS
+	UnexpectedErrorCode                 MErrorCode = 0
+	DatabaseErrorCode                   MErrorCode = 1
+	NotImplementedErrorCode             MErrorCode = 2
+	NothingChangedErrorCode             MErrorCode = 3
+	CannotGenerateAuthTokenErrorCode    MErrorCode = 4
+	CannotCreateValidationCodeErrorCode MErrorCode = 5
+	CannotReadFileErrorCode             MErrorCode = 6
+	CannotCreateRequestErrorCode        MErrorCode = 7
+	DoRequestErrorCode                  MErrorCode = 8
+
+	// 1000 -> 1999 | VALIDATION ERRORS
+	InvalidRequestErrorCode   MErrorCode = 1000
+	ParseJSONErrorCode        MErrorCode = 1001
+	ReadResponseBodyErrorCode MErrorCode = 1002
+
+	// 2000 -> 2099 | BLUESKY ERRORS
+	BskyAuthRequestErrorCode MErrorCode = 2000
+	BskyAuthErrorCode        MErrorCode = 2001
+	BskyAuthDecodeErrorCode  MErrorCode = 2002
+	BskyUploadBlobErrorCode  MErrorCode = 2003
+	BskyPostRequestErrorCode MErrorCode = 2004
+	BskyPostErrorCode        MErrorCode = 2005
+
+	// 2100 -> 2199 | TWITTER ERRORS
+
+	// 2200 -> 2299 | INSTAGRAM ERRORS
+
+	// 2300 -> 2399 | TELEGRAM CHANNEL ERRORS
+
+	// 2400 -> 2499 | TELEGRAM BOT ERRORS
+	CannotSendMessageToOwner MErrorCode = 2400
+
+	// 4000 -> 4999 | DATABASE ERRORS
+	NotFoundErrorCode   MErrorCode = 4000
+	UpdatePostErrorCode MErrorCode = 4001
+
+	// 5000 -> 5999 | AUTHORITATION ERRORS
+	UnauthorizedErrorCode MErrorCode = 5000
+
+	// 6000 -> 7999 | PERMISSION ERRORS
+	AccessDeniedErrorCode          MErrorCode = 6000
+	NotEnoughtPermissionsErrorCode MErrorCode = 6001
+)
+
+const (
+	AccessDeniedMessage string = "access denied"
+
+	CannotConnectToDatabaseMessage string = "cannot connect to database"
+	DatabaseConnectionEmptyMessage string = "database connection cannot be empty"
+	ServiceIDEmptyMessage          string = "service id cannot be empty"
+	RegisteredDomainsEmptyMessage  string = "registered domains cannot be empty"
+	SecretEmptyMessage             string = "secret cannot be empty"
+
+	TokenEmptyMessage   string = "token cannot be empty"
+	TokenInvalidMessage string = "invalid token"
+
+	FileTooLargeMessage string = "%s is too long; the maximum size is %dMB"
+
+	PasswordEmptyMessage                string = "password cannot be empty"
+	PasswordShortMessage                string = "password is short"
+	PasswordNoNumericMessage            string = "password must contain at least one numeric character"
+	PasswordNoSpecialCharacterMessage   string = "password must contain at least one special character"
+	PasswordNoLowercaseCharacterMessage string = "password must contain at least one uppercase character"
+	PasswordNoUppercaseCharacterMessage string = "password must contain at least one lowercase character"
+
+	EmailEmptyMessage string = "email cannot be empty"
+
+	UserEmptyMessage               string = "user cannot be empty"
+	UserNotFoundMessage            string = "user not found"
+	UserIDNegativeMessage          string = "user id must be positive"
+	UserCannotDeleteMessage        string = "cannot delete user"
+	UserCannotUpdateMessage        string = "cannot update user"
+	UserAlreadyExistsMessage       string = "user already exists"
+	UserProfilePictureEmptyMessage string = "no profile picture provided"
+
+	DeviceEmptyMessage          string = "device cannot be empty"
+	DeviceAddressEmptyMessage   string = "device address cannot be empty"
+	DeviceUserAgentEmptyMessage string = "device user agent cannot be empty"
+)
